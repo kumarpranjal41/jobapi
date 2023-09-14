39ad1d9519977f3653bc1546a20da1092656431d
kumarpranjal41 <you@example.com>
2023-09-14 10:34:30 +0530

    job update api sort by id

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.get('/', async(req , resp)=>{
     let data = await dbconnect();
-    data =await data.find().sort({ date: -1 }).toArray();
+    data =await data.find().sort({ id: -1 }).toArray();
     console.log(data);
     resp.send(data);
 });
